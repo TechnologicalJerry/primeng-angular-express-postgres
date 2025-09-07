d68df93de3bfa447ff109f384ed0a60b1752651b
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-07 22:18:52 +0530

    Create User.ts

---
+++ express-server/src/models/User.ts
@@ -0,0 +1,89 @@
+import { Model, DataTypes, Sequelize } from 'sequelize';
+import bcrypt from 'bcryptjs';
+import sequelize from '../config/database';
+import { User as UserInterface } from '../types';
+
+class User extends Model<UserInterface> implements UserInterface {
+  public id!: number;
+  public email!: string;
+  public password!: string;
+  public firstName!: string;
+  public lastName!: string;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  // Instance method to compare password
+  public async comparePassword(candidatePassword: string): Promise<boolean> {
+    return bcrypt.compare(candidatePassword, this.password);
+  }
+
+  // Instance method to hash password
+  public async hashPassword(): Promise<void> {
+    if (this.changed('password')) {
+      this.password = await bcrypt.hash(this.password, 12);
+    }
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+        notEmpty: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        len: [6, 255],
+      },
+    },
+    firstName: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [1, 100],
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [1, 100],
+      },
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'users',
+    modelName: 'User',
+    timestamps: true,
+    hooks: {
+      beforeSave: async (user: User) => {
+        await user.hashPassword();
+      },
+    },
+  }
+);
+
+export default User;
